3c72a10f4fb02c2321984204e23d9af605f80d06
Andreas Askjem <andaskjem@gmail.com>
2020-01-16 16:00:09 +0100

    In progress

---
+++ FindModeInArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindModeInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = {3, 6, 3, 2, 1, 3, 6, 5};
+            Array.Sort(myArray);
+
+            int currentStreak = 1;
+            int longestStreak = 1;
+            int valueOfLongestStreak = 0;
+            
+            for ( var i = 0; i < myArray.Length-1; i++ )
+            {
+                var num = myArray[i];
+                var nextNum = myArray[i + 1];
+                if (num == nextNum)
+                {
+                    currentStreak++;
+                }
+                else if (currentStreak > longestStreak)
+                {
+                    longestStreak = currentStreak;
+                    currentStreak = 1;
+                    valueOfLongestStreak = myArray[i];
+                }
+            }
+
+            Console.Write(valueOfLongestStreak);
+            Console.WriteLine(longestStreak);
+            //Console.WriteLine(myArray);
+        }
+    }
+}
