10255b69576208e47cb2c4bc5f7f0e90c5614d43
Andreas Askjem <andaskjem@gmail.com>
2020-01-21 15:54:54 +0100

    a

---
+++ TrePåRad/BoardModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace TrePåRad
+{
+    public class BoardModel
+    {
+        public int Height = 10;
+        public int Width = 20;
+
+        public char[,] BoardState =
+        {
+            { ' ', ' ', ' ' },
+            { ' ', ' ', ' ' },
+            { ' ', ' ', ' ' }
+
+        };
+
+        internal void SetCross(string position)
+        {
+            if (position.Length != 2)
+            {
+                Console.WriteLine("Not valid input!");
+                return;
+            }
+            var letter = position[0] - 'a';
+            var digit = position[1] - '0'-1;
+            if (letter > 2 || letter < 0 ||
+                digit  > 2 || digit  < 0)
+            {
+                Console.WriteLine("Not valid input!");
+                return;
+            }
+
+            if (IsTaken(letter, digit))
+            {
+                Console.WriteLine("This position is already taken!");
+                Console.Write("Skriv inn hvor du vil sette kryss (f.eks. \"a2\"): ");
+                position = Console.ReadLine();
+                SetCross(position);
+            }
+
+            PlaceInput(letter, digit, 'x');
+        }
+
+        internal void SetRandomCircle()
+        {
+            int letter;
+            int digit;
+            do
+            {
+                letter = Random.Next(0, 3);
+                digit = Random.Next(0, 3);
+            } while(IsTaken(letter, digit));
+            
+            PlaceInput(letter, digit, 'o');
+        }
+
+        private void PlaceInput(int letter, int digit, char symbol)
+        {
+            BoardState[letter, digit] = symbol;
+        }
+
+        private bool IsTaken(int letter, int digit)
+        {
+            return BoardState[letter, digit] != ' ';
+        }
+
+        private static readonly Random Random = new Random();
+
+        public char CheckIfWon()
+        {
+            char winner = ' ';
+            var b = BoardState;
+            if (b[0, 0] == b[0, 1] && b[0, 0] == b[0, 2] || //┌────
+                b[0, 0] == b[1, 0] && b[0, 0] == b[2, 0])   //│
+            {
+                winner = b[0, 0];
+            }
+            else if (b[2, 2] == b[2, 1] && b[2, 2] == b[2, 0] || //    │
+                     b[2, 2] == b[1, 2] && b[2, 2] == b[0, 2])   //────┘
+            {
+                winner = b[2, 2];
+            }
+            else if (b[1, 1] == b[1, 0] && b[1, 1] == b[1, 2] || // \ | /
+                     b[1, 1] == b[0, 1] && b[1, 1] == b[2, 1] || // ─ ┼ ─
+                     b[1, 1] == b[0, 0] && b[1, 1] == b[2, 2] || // / | \
+                     b[1, 1] == b[0, 2] && b[1, 1] == b[2, 0])
+            {
+                winner = b[1, 1];
+            }
+            else
+            {
+                for (var i = 0; i < 3; i++)
+                {
+                    for (var j = 0; j < 3; j++)
+                    {
+                        if (winner == ' ')
+                        {
+                            winner = 't';
+                        }
+                    }
+                }
+            }
+
+            return winner;
+        }
+    }
+}
---
+++ TrePåRad/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace TrePåRad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartNewGame();
+        }
+
+        private static void StartNewGame()
+        {
+            var game = new BoardModel();
+            Show(game);
+            var winner = ' ';
+            while (winner == ' ')
+            {
+                Console.Write("Skriv inn hvor du vil sette kryss (f.eks. \"a2\"): ");
+                var position = Console.ReadLine();
+                game.SetCross(position);
+                Show(game);
+                winner = game.CheckIfWon();
+                if(winner != ' ') continue;
+
+                Thread.Sleep(1000);
+                game.SetRandomCircle();
+                Show(game);
+                winner = game.CheckIfWon();
+            }
+
+            if (winner == 'x')
+            {
+                Console.WriteLine("You won! Play again? (y/n)");
+                var answer = Console.ReadLine();
+                if(answer=="y") StartNewGame();
+            }
+            else if (winner == 'o')
+            {
+                Console.WriteLine("You lost! Play again? (y/n)");
+                var answer = Console.ReadLine();
+                if (answer == "y") StartNewGame();
+            }
+            else if (winner == 't')
+            {
+                Console.WriteLine("It's a tie! Play again? (y/n)");
+                var answer = Console.ReadLine();
+                if (answer == "y") StartNewGame();
+            }
+        }
+
+        private static void Show(BoardModel game)
+        {
+            string[] board = {
+                 "  a b c",
+                 " ┌─────┐",
+                $"1│{game.BoardState[0,0]} {game.BoardState[0,1]} {game.BoardState[0,2]}│",
+                $"2│{game.BoardState[1,0]} {game.BoardState[1,1]} {game.BoardState[1,2]}│",
+                $"3│{game.BoardState[2,0]} {game.BoardState[2,1]} {game.BoardState[2,2]}│",
+                 " └─────┘"
+            };
+            foreach (var line in board)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
