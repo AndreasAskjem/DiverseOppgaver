c55bdc4a016944ee4dad6f84e1648885751661df
Andreas Askjem <andaskjem@gmail.com>
2020-01-23 16:02:21 +0100

    New project, new opportunities

---
+++ Players/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Players
+{
+    internal class Player
+    {
+        private string _name;
+        private int _points;
+
+        public Player(string name, int points)
+        {
+            _name = name;
+            _points = points;
+        }
+
+        internal void Play(Player otherPlayer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
