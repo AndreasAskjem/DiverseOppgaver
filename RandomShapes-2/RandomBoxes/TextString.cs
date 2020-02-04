93a040aab8ff980f4c7a1ca0372b1df361be29cc
AndreasAskjem <andreasa@getacademy.com>
2020-02-04 20:10:21 +0100

    a

---
+++ RandomShapes-2/RandomBoxes/TextString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RandomBoxes
+{
+    internal class TextString : Shape
+    {
+        private string _txt;
+
+        public TextString(int x, int y,string txt, Random random)
+        {
+            X = x;
+            Y = y;
+            _txt = txt;
+        }
+
+        public override string GetCharacter(int row, int col)
+        {
+            if (row != Y || col < X || col >= X + _txt.Length) return null;
+            var index = col - X;
+            return _txt[index].ToString();
+        }
+    }
+}
