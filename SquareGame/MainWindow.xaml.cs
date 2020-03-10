5f4e1572448fbfb5daf86fcadec6c9b9cd4c4c80
Andreas Askjem <andaskjem@gmail.com>
2020-03-10 13:19:42 +0100

    a

---
+++ SquareGame/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SquareGame
+{
+    public partial class MainWindow : Window
+    {
+
+        private System.Windows.Threading.DispatcherTimer gameTickTimer = new System.Windows.Threading.DispatcherTimer();
+        public Player Player;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            gameTickTimer.Tick += GameTickTimer_Tick;
+            Player.Pos {
+                var x = PlayerRectangle.GetValue(Canvas.BottomProperty);
+            };
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            DrawGameArea();
+
+        }
+
+        private void DrawGameArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GameTickTimer_Tick(object sender, EventArgs e)
+        {
+            MovePlayer();
+        }
+
+        private void MovePlayer()
+        {
+            if (Player.PositionX < 0)
+            {
+                PlayerRectangle.Margin.Bottom = 0;
+            }
+        }
+    }
+}
---
+++ SquareGame/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using Point = System.Drawing.Point;
+using ValidationResult = System.ComponentModel.DataAnnotations.ValidationResult;
+
+
+namespace SquareGame
+{
+    public class Player
+    {
+        private int size;
+        public int Size
+        {
+            get { return Application.Current.Resources[PlayerRectangle]; }
+            set { size = value; }
+        }
+
+        public object Pos;
+
+        private int posX;
+
+        
+
+        public double VerticalSpeed { get; set; }
+        public double HorizontalSpeed { get; set; }
+
+        //public int MaxHeight = Display.
+
+        public UIElement UIElement { get; set; }
+        public Player()
+        {
+            Size = 50;
+            VerticalSpeed = 0;
+            HorizontalSpeed = 0;
+        }
+
+    }
+}
